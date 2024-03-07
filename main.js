0e17c209a80c74f18126843250ba7bddf4c300a1
sachan12 <sachinnanda427@gmail.com>
2024-03-07 10:35:25 -0800

    add index.html and main.js files

---
+++ main.js
@@ -0,0 +1,26 @@
+// Chapter - 12 ; 
+//  var x = prompt("where does pope live?");
+// if (x === "vatician" );{
+//     alert("correct")
+// }
+// if (x !="vatician");{
+//     alert("wrong answer")
+// }
+// if (x === "vatician" ){
+//          alert("correct!")
+// }
+// else {
+//     alert("wrong answer");
+// }
+// var correctAnswer = "Vatician";
+// if (x === correctAnswer){
+//     alert("Correct!");
+// }
+// else if (x === "Rome"){
+//     alert("InCorrect but Close!");
+// }
+// else {
+//     alert("InCorrect!");
+// }
+// Not Defined error hai or undefined error nahie hai:
+
